4ec7611db7e5a21fe62866c4244920db57ad3bb8
Alex112292 <57815789+Alex112292@users.noreply.github.com>
2021-02-09 19:46:45 -0500

    Groudwork for the program

--- src/vendingMachine.java
+++ src/vendingMachine.java
@@ -1,4 +1,12 @@
+import java.util.Scanner;
+
 public class vendingMachine
 {
-    
+    public static void main(String[] args)
+    {
+        System.out.println("Please insert money.");
+        System.out.println("Snickers - $1.00, Chips - $2.00, Gatorade - $2.00, Reese's - $1.00, Trail Mix - $3.00, \nHershey's - $2.00, Granola Bar - $2.00");
+        Scanner input = new Scanner(System.in);
+        int amount = input.nextInt();
+    }
 }
