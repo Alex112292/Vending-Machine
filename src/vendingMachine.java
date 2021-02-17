7651e9ef000bac754fcc1e30acc3678ac7a6d99d
cacooper21 <58480492+cacooper21@users.noreply.github.com>
2021-02-17 12:35:34 -0500

    Made the user be able to insert as many dollar bills as they'd like as well as select their snack. Also put in instructions.

--- src/vendingMachine.java
+++ src/vendingMachine.java
@@ -4,9 +4,16 @@ public class vendingMachine
 {
     public static void main(String[] args)
     {
-        System.out.println("Please insert money.");
-        System.out.println("Snickers - $1.00, Chips - $2.00, Gatorade - $2.00, Reese's - $1.00, Trail Mix - $3.00, \nHershey's - $2.00, Granola Bar - $2.00");
+        System.out.println("Welcome to the best snack machine of your life! This machine only accepts $1 bills. Please insert money.");
+        System.out.println("A1 Snickers - $1.00, A2 Chips - $2.00,  B4 Gatorade - $2.00,  C7 Reese's - $1.00, A9 Trail Mix - $3.00, \n B5 Hershey's - $2.00, G8 Granola Bar - $2.00");
+        System.out.println("How many $1 bills would you like to enter?");
         Scanner input = new Scanner(System.in);
         int amount = input.nextInt();
+        System.out.println("You have put in " + amount + " dollar bills");
+        System.out.println("Please enter your selected snack.");
+        Scanner input2 = new Scanner(System.in);
+        String snack = input2.nextLine();
+        System.out.println("You have selected " + snack + ". One moment please.");
+        
     }
 }
